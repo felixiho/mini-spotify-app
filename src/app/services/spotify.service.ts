6c72b8b2795136b8f370bfed5e9bec23eca5ea5b
David León <delc82@gmail.com>
2017-08-30 16:25:08 -0500

    More optimizations

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -12,34 +12,37 @@ export class SpotifyService{
   constructor(private _http:Http){ }
 
   searchMusic(str:string, type='artist'){
-    let headers = new Headers();
-    headers.append('Authorization', `Bearer ${this.authToken}`);
+    const options = this.getOptions();
     this.searchUrl = `https://api.spotify.com/v1/search?q=${str}&offset=0&limit=20&type=${type}&market=US`;
-    return this._http.get(this.searchUrl, { headers })
+    return this._http.get(this.searchUrl, options)
       .map(res => res.json());
   }
 
   getArtist(id:string){
-    let headers = new Headers();
-    headers.append('Authorization', `Bearer ${this.authToken}`);
+    const options = this.getOptions();
     this.artistUrl = `https://api.spotify.com/v1/artists/${id}`;
-    return this._http.get(this.artistUrl, { headers })
+    return this._http.get(this.artistUrl, options)
       .map(res => res.json());
   }
 
   getAlbums(artistId:string){
-    let headers = new Headers();
-    headers.append('Authorization', `Bearer ${this.authToken}`);
+    const options = this.getOptions();
     this.albumsUrl = `https://api.spotify.com/v1/artists/${artistId}/albums`;
-    return this._http.get(this.albumsUrl, { headers })
+    return this._http.get(this.albumsUrl, options)
       .map(res => res.json());
   }
 
   getAlbum(id:string){
-    let headers = new Headers();
-    headers.append('Authorization', `Bearer ${this.authToken}`);
+    const options = this.getOptions();
     this.albumUrl = `https://api.spotify.com/v1/albums/${id}`;
-    return this._http.get(this.albumUrl, { headers })
+    return this._http.get(this.albumUrl, options)
       .map(res => res.json());
+  }
+
+  private getOptions() {
+    let header = new Headers();
+    header.append('Authorization', `Bearer ${this.authToken}`);
+    let options = new RequestOptions({ headers: header });
+    return options;
   }
 }
